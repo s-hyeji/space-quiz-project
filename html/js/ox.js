ff0d5d909d67450afc7cda7224b2475809a14ed4
khwoo91 <khyunwoo1991@gmail.com>
2024-05-21 23:34:01 +0900

    111

--- html/js/ox.js
+++ html/js/ox.js
@@ -21,7 +21,7 @@ window.addEventListener('load', function () {
 
 
   // ▼ OX퀴즈 영역======================================================= //
-  var ox_container = this.document.querySelector('[data-quiz="ox"]')
+  var ox_container = this.document.querySelectorAll('[data-quiz="ox"]')
   var ox_btn = this.document.querySelectorAll('[data-quiz="ox"] [class*="btn_icon"]')
 
 
@@ -31,8 +31,17 @@ window.addEventListener('load', function () {
       // 정답 클릭시
       if (ox_btn.attributes.length === 2) {
         console.log('정답!')
-        ox_container.classList.add('complete')
+
         this.classList.add('on')
+
+
+        // 컴플리트 해결해야함!
+        for (let i = 0; i < ox_container.self; i++) {
+          ox_container[i].classList.add('complete')
+        }
+
+
+
 
         for (let i = 0; i < quizBox.length; i++) {
           quizBox[i].classList.add('off')
