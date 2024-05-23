286fd4696034aec6d09c704f88e8420f9c209a6f
s-hyeji <shj981004@gmail.com>
2024-05-23 13:43:46 +0900

    hj

--- html/js/quiz.js
+++ html/js/quiz.js
@@ -9,47 +9,61 @@ var goodJop_popup = this.document.querySelector('.popup_container .goodJop_popup
 
 // 스타트버튼 클릭시
 start_btn.addEventListener('click', function () {
-	quiz_p[0].classList.add('on')
-	start_page.classList.add('off')
+    quiz_p[0].classList.add('on')
+    start_page.classList.add('off')
 })
+
+
+
+// 해당페이지가 마지막인지 구분
+function lastPage() {
+    let thisPage = document.querySelector('[class*="quiz_"].on')
+    let lastPage = quiz_p[quiz_p.length - 1];
+    // 정답을 누르면 다음버튼 오픈
+    if (thisPage !== lastPage) {
+        next();
+    } else {
+        last();
+    }
+    // 마지막페이지이면 참잘했어요팝업 오픈
+}
 
 // console.log()
 if (document.querySelector('.popup_container .nextBtn') === null) {
 } else {
-	// 다음퀴즈 버튼 클릭시
-	next_btn.addEventListener('click', function () {
-		nextPage();
-	})
+    // 다음퀴즈 버튼 클릭시
+    next_btn.addEventListener('click', function () {
+        nextPage();
+    })
 }
-
 
 
 // 다음 퀴즈만! on
 function nextPage() {
-	let thisPage = document.querySelector('[class*="quiz_"].on')
-	let lastPage = quiz_p[quiz_p.length - 1];
-	if (thisPage !== lastPage) {
-		console.log("해당페이지 확인", thisPage);
-		popup.classList.remove('dim')
-		next_btn.classList.remove('on')
-		setTimeout(() => {
-			quizBox.forEach(this_e => { this_e.classList.remove('off') });
-		}, 1000);
+    let thisPage = document.querySelector('[class*="quiz_"].on')
+    let lastPage = quiz_p[quiz_p.length - 1];
+    if (thisPage !== lastPage) {
+        console.log("해당페이지 확인", thisPage);
+        popup.classList.remove('dim')
+        next_btn.classList.remove('on')
+        setTimeout(() => {
+            quizBox.forEach(this_e => { this_e.classList.remove('off') });
+        }, 1000);
 
-		thisPage.classList.remove('on')
-		thisPage.nextElementSibling.classList.add('on')
+        thisPage.classList.remove('on')
+        thisPage.nextElementSibling.classList.add('on')
 
-		return;
-	} else {
-		console.log("마지막 문제입니다.");
-	}
+        return;
+    } else {
+        console.log("마지막 문제입니다.");
+    }
 }
 
 // 참잘했어요 공통 함수
 function goodJopPopup() {
-	let lastPage = quiz_p[quiz_p.length - 1];
-	if (lastPage.classList.contains('complete')) {
-		popup.classList.add('dim')
-		goodJop_popup.classList.add('on')
-	}
+    let lastPage = quiz_p[quiz_p.length - 1];
+    if (lastPage.classList.contains('complete')) {
+        popup.classList.add('dim')
+        goodJop_popup.classList.add('on')
+    }
 }
