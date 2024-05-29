7c00cd3619473c22190decb098110a5b7ebccaf7
khwoo91 <khyunwoo1991@gmail.com>
2024-05-29 18:31:15 +0900

    1111

--- html/js/input.js
+++ html/js/input.js
@@ -1,21 +1,38 @@
 
 // 사용자 정답과 비교하기
+var quizPage = document.querySelector('.quiz_1')
 const answer = document.querySelector(".answerBtn");
 const userinput = document.querySelectorAll("input")
 
 answer.addEventListener("click", function () {
   inputvaluecheck();
+  // inputAddComplete();
+  for (let i = 0; i < userinput.length; i++) {
+    console.log('>>>>>', userinput[i].classList.length);
+    if (userinput[i].classList.contains('correct') == true) {
+      quizPage.classList.add('complete')
+      // setTimeout(() => { goodJopPopup(); }, 500);
+      console.log('컴플리트 붙음');
+    } else {
+      console.log('컴플리트 안붙음');
+    }
+  }
 })
+
+
 
 // 인풋 value 체크 함수
 function inputvaluecheck() {
   let emptyCount = 0;
+
   for (let i = 0; i < userinput.length; i++) {
     if (userinput[i].value.trim() === "") {
       emptyCount++;
-      console.log(emptyCount)
+      // console.log(emptyCount)
     };
   };
+
+
 
   //  하나 이상시 체킹하기
   if (emptyCount >= 1) {
@@ -27,8 +44,10 @@ function inputvaluecheck() {
   }
 }
 
-// 수진님 여기다가 정답체킹 해주세요 
-//정답시 팝업함수 뜨고 틀리면 ..( 틀렸단 알림과) + 인풋 value 지우기
+
+
+
+// ▼ 수진 :: 정답시 팝업함수 뜨고 틀리면 ..( 틀렸단 알림과) + 인풋 value 지우기
 function valueCompare() {
   console.log('정답을 모두 입력했음=================================');
   let userinputVal = [];
@@ -45,15 +64,20 @@ function valueCompare() {
   for (i = 0; i < userinputAns.length; i++) {
     if (userinputAns[i] != userinputVal[i]) {
       console.log('오답');
-      console.log(userinput[i].value);
+      // console.log(userinput[i].value);
       userinput[i].value = "";
       e = e + 1;
     }
     if (e == 0) {
-      console.log('정답~~');
+      console.log('정답');
+      userinput.forEach(function (INPUT) {
+        // console.log('>>>>>>>>>>>인풋 정답입력',INPUT.value);
+        if (INPUT.value == true) {
+        } else {
+          INPUT.classList.add('correct')
+        }
+      })
     }
-
-
   }
 
   // 힌트 함수
@@ -64,3 +88,17 @@ function valueCompare() {
     return cho[initialIndex];
   }
 }
+
+
+// function inputAddComplete() {
+//   for (let i = 0; i < userinput.length; i++) {
+//     console.log('>>>>>', userinput[i].classList.contains('correct'));
+//     if (userinput[i].classList.contains('correct') === true) {
+//       quizPage.classList.add('complete')
+//       setTimeout(() => { goodJopPopup(); }, 500);
+//       console.log('컴플리트 붙음');
+//     } else {
+//       console.log('컴플리트 안붙음');
+//     }
+//   }
+// }
