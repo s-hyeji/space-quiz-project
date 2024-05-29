d764fcfa40b79b9df6bf9bbd0b38f185b85843d7
khwoo91 <khyunwoo1991@gmail.com>
2024-05-29 16:10:41 +0900

    111

--- html/js/input.js
+++ html/js/input.js
@@ -30,36 +30,37 @@ function inputvaluecheck() {
 // 수진님 여기다가 정답체킹 해주세요 
 //정답시 팝업함수 뜨고 틀리면 ..( 틀렸단 알림과) + 인풋 value 지우기
 function valueCompare() {
-console.log('정답을 모두 입력했음=================================');
-let userinputVal = [];
-let userinputAns = [];
+  console.log('정답을 모두 입력했음=================================');
+  let userinputVal = [];
+  let userinputAns = [];
 
-  userinput.forEach(element => {  
+  userinput.forEach(element => {
     userinputVal.push(element.value);
     userinputAns.push(element.getAttribute('answer'));
   });
-      
+
   console.log(userinputAns);
   console.log(userinputVal);
   let e = 0;
-  for(i = 0; i<userinputAns.length; i++) {
+  for (i = 0; i < userinputAns.length; i++) {
     if (userinputAns[i] != userinputVal[i]) {
       console.log('오답');
       console.log(userinput[i].value);
       userinput[i].value = "";
-      e= e + 1;
+      e = e + 1;
+    }
+    if (e == 0) {
+      console.log('정답~~');
+    }
+
+
   }
-  if (e==0) {
-    console.log('정답~~');
+
+  // 힌트 함수
+  function getHangulInitial(char) {
+    const cho = ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"];
+    const code = char.charCodeAt(0) - 44032;
+    const initialIndex = Math.floor(code / 588);
+    return cho[initialIndex];
   }
-
-
-}
-
-// 힌트 함수
-function getHangulInitial(char) {
-  const cho = ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"];
-  const code = char.charCodeAt(0) - 44032;
-  const initialIndex = Math.floor(code / 588);
-  return cho[initialIndex];
 }
